b9b2ddb72d427e98568d118274e72906aa903f6d
Kevin Pennartz <kpennart@uncc.edu>
2019-03-18 21:04:51 +0000

    Add validations and error messages for name_of_the_game, game_username, and text

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
     validates :title, presence: true, length: { minimum: 2 }
+    validates :title_of_the_game, presence: true, length: { minimum: 2 }
+    validates :game_username, presence: true, length: { minimum: 2 }
+    validates :text, presence: true, length: { minimum: 10 }
 end
