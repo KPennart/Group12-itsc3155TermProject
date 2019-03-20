dc913cc63f6c2e223076752ee5edd15f0c2daa44
Kevin Pennartz <kpennart@uncc.edu>
2019-03-20 03:19:02 +0000

    Add associated comments model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    has_many :comments
     validates :title, presence: true, length: { minimum: 2 }
     validates :title_of_the_game, presence: true, length: { minimum: 2 }
     validates :game_username, presence: true, length: { minimum: 2 }
