37c25d0edd0ea5323579782359253efef67cfc3c
Johannes Lee <jlee310@uncc.edu>
2019-03-25 17:42:01 +0000

    fixed the comment invalid foreign key

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :delete_all
     validates :title, presence: true, length: { minimum: 2 }
     validates :title_of_the_game, presence: true, length: { minimum: 2 }
     validates :game_username, presence: true, length: { minimum: 2 }
