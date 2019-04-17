d4d8a3899c61e9df4e5ce258f5a325a7311bbbaf
Kevin Pennartz <kpennart@uncc.edu>
2019-04-17 20:51:33 +0000

    Testing changes in comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,19 @@ class CommentsController < ApplicationController
         @post = Post.find_by_id(params[:post_id])
         @comment = @post.comments.create(comment_params)
         @comment.commenter = current_user.name
+        @comment.date = Time.now
         redirect_to post_path(@post)
     end
+    
+    def destroy
+        @comment = Post.find_by_id(params[:post_id])
+        @comment.destroy
+        
+        redirect_to posts_path
+    end
+    
     private
         def comment_params
-            params.require(:comment).permit(:game_username, :body)
+            params.require(:comment).permit(:commenter, :game_username, :body)
         end
 end
