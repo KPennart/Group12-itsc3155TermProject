79030c638cf4fc112f75518117fbe11271a975c0
Kevin Pennartz <kpennart@uncc.edu>
2019-04-24 16:38:23 +0000

    Testing comment changes

--- app/controllers/comments_controller_backup.rb
+++ app/controllers/comments_controller_backup.rb
@@ -6,6 +6,6 @@ class CommentsController < ApplicationController
     end
     private
         def comment_params
-            params.require(:comment).permit(:commenter, :body)
+            params.require(:comment).permit(:commenter, :game_username, :body)
         end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :post
+    belongs_to :post
+    validates :game_username, presence: true, length: { minimum: 2 }
+    validates :body, presence: true, length: { minimum: 10 }
 end
