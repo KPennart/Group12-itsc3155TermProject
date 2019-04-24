d32c86328aab119b7a052b19b81113acacd3e4dc
Kevin Pennartz <kpennart@uncc.edu>
2019-04-24 17:43:09 +0000

    Only people who are logged in can see usernames

--- app/controllers/comments_controller_backup.rb
+++ app/controllers/comments_controller_backup.rb
@@ -2,10 +2,11 @@ class CommentsController < ApplicationController
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
+        @comment.date = (Time.now)
         redirect_to post_path(@post)
     end
     private
         def comment_params
-            params.require(:comment).permit(:commenter, :game_username, :body)
+            params.require(:comment).permit(:game_username, :body)
         end
 end
