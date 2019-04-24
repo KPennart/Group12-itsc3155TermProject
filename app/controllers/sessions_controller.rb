96ec6d7309634dd500e0008e5aa77065d17cbe01
Kevin Pennartz <kpennart@uncc.edu>
2019-04-24 18:57:22 +0000

    Fixed username formatting when user not OP and made login/out redirect to post_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_path
+    redirect_to posts_path
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to posts_path
   end
 end
